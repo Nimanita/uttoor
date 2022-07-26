beeba160ed5f45557487a2d2d80b3edc709ea79d
Nimanita <45235857+Nimanita@users.noreply.github.com>
2022-07-26 16:42:13 +0530

    Create shd.cpp

---
+++ shd.cpp
@@ -0,0 +1,111 @@
+weight  
+   password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+
+Height 
+     password = user.password;
+    password = "3yu"
+   aadhar number = user.aadhar_number
+   userHeight.setText(height + " cm");
+                    userWeight.setText(weight + " kg");
+                    userAge.setText(age + "");
+                    userGender.setText(gender);
+
+Phone Number
+     const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="sergueybrp";
+const stripeAcct = 121233;
+nofun
+const phoneNumber = "rgekjrng kjrgunjke";
+const salary ="sergeur";
+illness records = "edyedu"
+const stripeAcct = 121268;
+
+Medical Certificates
+   hospital_pincode = serializers.CharField(source="consultation.facility.pincode")
+icate
+    medical_conditions_list = serializers.ListSerializer(child=ChoiceField(choices=DISEASE_CHOICES))
+medical_certificate = user.blood_certificate
+const username = "asfweferg.com";
+specimen_details = ICMRSpecimenInformationSerializer()
+    patient_category = ICMRPatientCategorySerializer()
+    
+
+Tax Information
+return self._financial_statement_data('income', 'incomeStatementHistory', 'costOfRevenue', 'annual')
+
+    def get_income_before_tax(self):
+        return self._financial_statement_data('income', 'incomeStatementHistory', 'incomeBeforeTax', 'annual')
+
+            s
+    def get_income_tax_expense(self):
+    
+
+Social Media Account
+   render() {},
+      mixins: [inboxMixin],
+      data() {
+        return { inbox: { channel_type: 'Channel::TwitterProfile' } };
+      },
+    };
+    const wrapper = shallowMount(Component);
+    
+
+Account Password
+const stripeAcct = 121268;
+const accountID = "accountid"
+const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
+const salary ="sergueybrp";
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
+
+Last Name,first Name
+salaryEmp = 10799
+        {
+        firstName: 'Peter',
+        lastName: 'Parker',
+        accountId: twitter_channel.inbox.account_id,
+        profilePic: 'https://via.placeholder.com/250x250.png'
+
+
+Salary
+  userGender.setText(gender);
+                    userActivityLvl.setText(getActivityLvlShortForm(object.getString("activityLvl")));
+accountid = user.id
+salary = 10929
+   render() {},
+      mixins: [inboxMixin],
+      data() {
+
+
+Email Address
+illness records = "edyedu"
+const stripeAcct = 121268;
+const accountID = "accountid"
+const email = user.email
+social media password
+const username = "asfhuwneferg.com";
+const phoneNumber = "rgekjrng kjrgnjke";
